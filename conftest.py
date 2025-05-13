02f3914c87c73db27ccae4eaa711b14cb6429a44
Mykyta <noobssinside@gmail.com>
2025-05-13 21:59:05 +0200

    need to fix conftest

--- conftest.py
+++ conftest.py
@@ -5,11 +5,11 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture
 def driver():
     options = Options()
-    #options.add_argument("--headless")
-    #options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    # Без webdriver-manager
+    # No webdriver-manager
     driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
