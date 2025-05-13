0865b4a16b4385d42fbb5620d4f9dca67ea7071b
Taras Halatsiuk <taras.halatsiuk@polavis.de>
2025-05-13 20:59:14 +0300

    test 1

--- conftest.py
+++ conftest.py
@@ -1,16 +1,15 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
-    service = Service(ChromeDriverManager().install())
-    options = webdriver.ChromeOptions()
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    options.add_argument("--incognito")
-    driver = webdriver.Chrome(service=service, options=options)
-
+    # Без webdriver-manager
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
